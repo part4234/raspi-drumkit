6f41c8613b57320cf86225ee552d13e23b19897c
Particles <particles2046@gmail.com>
2021-07-20 19:44:32 +0100

    fix formatting issue

--- helpers/mixer.py
+++ helpers/mixer.py
@@ -5,22 +5,23 @@ path = '/home/pi/Documents/Project/audio/'
 sample = 'beep.wav'
 low_acoustic_snare = 'low-acoustic-snare-sound.wav'
 
+
 class Mixer:
-  def __init__(self):
-    print('[Mixer] init')
-    mixer.init(frequency=44100, size=-16, channels=1, buffer=512)
-    self.loadSound()
+    def __init__(self):
+        print('[Mixer] init')
+        mixer.init(frequency=44100, size=-16, channels=1, buffer=512)
+        self.loadSound()
 
-  def loadSound(self):
-    self.sample = mixer.Sound(path + sample)
-    self.snare = mixer.Sound(path + low_acoustic_snare)
+    def loadSound(self):
+        self.sample = mixer.Sound(path + sample)
+        self.snare = mixer.Sound(path + low_acoustic_snare)
 
-  def playSample(self, volume=0.5):
-    # print('[Mixer] sample', volume)
-    self.sample.set_volume(volume)
-    self.sample.play()
+    def playSample(self, volume=0.5):
+        # print('[Mixer] sample', volume)
+        self.sample.set_volume(volume)
+        self.sample.play()
 
-  def playSnare(self, volume=0.5):
-    # print('[Mixer] snare', volume)
-    self.snare.set_volume(volume)
-    self.snare.play()
+    def playSnare(self, volume=0.5):
+        # print('[Mixer] snare', volume)
+        self.snare.set_volume(volume)
+        self.snare.play()
--- helpers/plot.py
+++ helpers/plot.py
@@ -5,22 +5,21 @@ from .peak_detect import PeakDetect
 
 
 def plot(data, alg: PeakDetect):
-  print('[Plot] plotting graphs...')
-  x = np.arange(1, len(data)+1)
-  avgFilter = np.asarray(alg.avgFilter)
-  stdFilter = np.asarray(alg.stdFilter)
+    print('[Plot] plotting graphs...')
+    x = np.arange(1, len(data)+1)
+    avgFilter = np.asarray(alg.avgFilter)
+    stdFilter = np.asarray(alg.stdFilter)
 
-  plt.subplot(211)
-  plt.plot(x, data)
-  plt.plot(x, alg.avgFilter, color="cyan", lw=2)
-  plt.plot(x, avgFilter + alg.threshold * stdFilter, color="green", lw=2)
-  plt.plot(x, avgFilter - alg.threshold * stdFilter, color="green", lw=2)
+    plt.subplot(211)
+    plt.plot(x, data)
+    plt.plot(x, alg.avgFilter, color="cyan", lw=2)
+    plt.plot(x, avgFilter + alg.threshold * stdFilter, color="green", lw=2)
+    plt.plot(x, avgFilter - alg.threshold * stdFilter, color="green", lw=2)
 
-  # plt.subplot(312)
-  # plt.step(x, alg.signals, color="red", lw=2)
-  # plt.ylim(-1.5, 1.5)
+    # plt.subplot(312)
+    # plt.step(x, alg.signals, color="red", lw=2)
+    # plt.ylim(-1.5, 1.5)
 
-  plt.subplot(212)
-  plt.step(x, alg.strengths, color="purple", lw=2)
-  plt.show()
-  
+    plt.subplot(212)
+    plt.step(x, alg.strengths, color="purple", lw=2)
+    plt.show()
