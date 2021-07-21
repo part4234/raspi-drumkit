94505f66e953c0e174bc5c520a75af8dcf4317b2
Particles <particles2046@gmail.com>
2021-07-21 13:56:29 +0100

    refactor load config

--- detect_hit.py
+++ detect_hit.py
@@ -1,86 +1,101 @@
+import time
+
+import yaml
+from yaml.loader import SafeLoader
+
 from helpers.adc_reader import AdcReader
 from helpers.mixer import Mixer
-import time
 
 
 def main():
-    # init
-    mixer = Mixer()
-    adc = AdcReader()
+    ctrl = DetectHit()
+    ctrl.start()
 
-    # settings
-    lag = 50
-    threshold_scale = 1.1
-    threshold_buffer = 50
-    peak_scale = 1.1
-    peak_buffer = 100
-    volume_lowest = 0.1
-    volume_scale = 1.2
-    cal_time = 5
+class DetectHit:
+    def __init__(self):
+        self.mixer = Mixer()
+        self.adc = AdcReader()
+        self.load_config()
 
-    # calibration
-    input('Press Enter to start calibration.')
-    threshold = get_threshold(adc, threshold_scale, threshold_buffer, cal_time)
-    peak = get_peak(adc, peak_scale, peak_buffer, cal_time)
+        # calibration
+        input('Press Enter to start calibration.')
+        self.threshold = self.get_threshold()
+        self.peak = self.get_peak()
 
-    # init counts
-    prev_count = 0
-    count = 0
-    hit_count = 0
-    hit_strength = 0
+    def load_config(self):
+        self.lag = 50
+        self.threshold_scale = 1.1
+        self.threshold_buffer = 50
+        self.peak_scale = 1.1
+        self.peak_buffer = 100
+        self.volume_lowest = 0.1
+        self.volume_scale = 1.2
+        self.cal_time = 5
+        # with open('config.yml') as file:
+        #     config = yaml.load(file, Loader=SafeLoader)
+        
 
-    while True:
-        if count > 999999:
-            count = 0
-        else:
-            count += 1
+    def calibrate(self):
+        self.threshold = self.get_threshold()
+        self.peak = self.get_peak()
 
-        value = adc.chan.value
-        if value > threshold:
-            hit_strength = max(hit_strength, value)
+    def get_threshold(self):
+        print('Calibrating threshold...')
+        print('Please leave the sensor idle.')
 
-            if count - prev_count > lag:  # is a hit
-                # play sound
-                volume = (value - threshold) * volume_scale / (peak - threshold)
-                if volume < volume_lowest:
-                    volume = volume_lowest
-                elif volume > 1.0:
-                    volume = 1.0
-                mixer.playSnare(volume)
+        max_val = 0
+        t_end = time.time() + self.cal_time
+        while time.time() < t_end:
+            max_val = max(max_val, self.adc.chan.value)
+        threshold = max_val * self.threshold_scale + self.threshold_buffer
 
-                # update counts
-                hit_count += 1
-                print(f'{hit_count}: {hit_strength}, {volume}')
-                prev_count = count
-                hit_strength = 0
+        print('Threshold:', threshold)
+        return threshold
 
+    def get_peak(self):
+        print('Calibrating peak...')
+        print('Please hit with hardest strength.')
 
-def get_threshold(adc: AdcReader, scale, buffer, cal_time=5):
-    print('Calibrating threshold...')
-    print('Please leave the sensor idle.')
+        max_val = 0
+        t_end = time.time() + self.cal_time
+        while time.time() < t_end:
+            max_val = max(max_val, self.adc.chan.value)
+        peak = max_val * self.peak_scale + self.peak_buffer
 
-    max_val = 0
-    t_end = time.time() + cal_time
-    while time.time() < t_end:
-        max_val = max(max_val, adc.chan.value)
-    threshold = max_val * scale + buffer
+        print('Limit:', peak)
+        return peak
 
-    print('Threshold:', threshold)
-    return threshold
+    def start(self):
+        prev_count = 0
+        count = 0
+        hit_count = 0
+        hit_strength = 0
 
+        while True:
+            if count > 999999:
+                count = 0
+            else:
+                count += 1
 
-def get_peak(adc: AdcReader, scale, buffer, cal_time=5):
-    print('Calibrating peak...')
-    print('Please hit with hardest strength.')
+            value = self.adc.chan.value
+            if value > self.threshold:
+                hit_strength = max(hit_strength, value)
 
-    max_val = 0
-    t_end = time.time() + cal_time
-    while time.time() < t_end:
-        max_val = max(max_val, adc.chan.value)
-    peak = max_val * scale + buffer
+                if count - prev_count > self.lag:  # is a hit
+                    # play sound
+                    volume = (value - self.threshold) * \
+                        self.volume_scale / (self.peak - self.threshold)
+                    if volume < self.volume_lowest:
+                        volume = self.volume_lowest
+                    elif volume > 1.0:
+                        volume = 1.0
+                    self.mixer.playSnare(volume)
 
-    print('Limit:', peak)
-    return peak
+                    # update counts
+                    hit_count += 1
+                    print(f'{hit_count}: {hit_strength}, {volume}')
+                    prev_count = count
+                    hit_strength = 0
 
 
 if __name__ == '__main__':
